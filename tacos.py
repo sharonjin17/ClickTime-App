8b223a8168539078ba60d0f8db4d8670d36236f9
Sharon <sharonjin17@gmail.com>
2018-11-26 15:36:15 -0500

    tacos.py
    Updated

--- tacos.py
+++ tacos.py
@@ -1,51 +1,38 @@
 from flask import Flask, render_template
+import requests
 
-shells, baseLayers, mixins, condiments, seasonings = []
+shells = []
+baseLayers = []
+mixins = []
+seasonings = []
+condiments = []
 
 shell_arr = requests.get("https://tacos-ocecwkpxeq.now.sh/shells").json()
 baseLayer_arr = requests.get("https://tacos-ocecwkpxeq.now.sh/baseLayers").json()
 mixin_arr = requests.get("https://tacos-ocecwkpxeq.now.sh/mixIns").json()
-condiment_arr = requests.get("https://tacos-ocecwkpxeq.now.sh/baseLayers").json()
-seasoning_arr = requests.get("https://tacos-ocecwkpxeq.now.sh/baseLayers").json()
+condiment_arr = requests.get("https://tacos-ocecwkpxeq.now.sh/condiments").json()
+seasoning_arr = requests.get("https://tacos-ocecwkpxeq.now.sh/seasonings").json()
 
 for shell_dict in shell_arr:
-    for s in shell_dict:
-        if s == "name":
-            shells.append(s)
+    shells.append(shell_dict.get("name"))
 
 for baseLayer_dict in baseLayer_arr:
-    for b in baseLayer_dict:
-        if b == "name":
-            baseLayers.append(b)
+    baseLayers.append(baseLayer_dict.get("name"))
             
 for mixin_dict in mixin_arr:
-    for m in mixin_dict:
-        if m == "name":
-            mixins.append(m)
+    mixins.append(mixin_dict.get("name"))
 
 for seasoning_dict in seasoning_arr:
-    for sn in seasoning_dict:
-        if sn == "name":
-            seasonings.append(sn)
+    seasonings.append(seasoning_dict.get("name"))
             
 for condiment_dict in condiment_arr:
-    for c in condiment_dict:
-        if c == "name":
-            condiments.append(c)
-
-print(condiments)
-print(mixins)
-print(baseLayers)
+    condiments.append(condiment_dict.get("name"))
             
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def main():
-    return render_template('index.html')
-
-@app.route('/test')
-def test(query):
-    return '<h1>You are on the test page.</h1>'
+    return render_template('index.html', shells = shells, baseLayers = baseLayers, mixins = mixins, condiments = condiments, seasonings = seasonings, tacos = [])
 
 if __name__ == '__main__':
     app.run()
